8f3913c2267d04b6c7d2753909296d1b33a25728
leodelafiori <leodelafiori@users.noreply.github.com>
2018-04-27 20:58:42 -0300

    Criação do ambiente inicial

---
+++ Assets/Scripts/Camera_follow_player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_follow_player : MonoBehaviour {
+
+    public Transform playerTransform;
+    public Transform mainCameraTransform = null;
+    private Vector3 cameraOffset = Vector3.zero;
+
+
+	// Use this for initialization
+	void Start () {
+
+        mainCameraTransform = Camera.main.transform;
+
+        // Pega a distancia player camera pelo camera offset subtraindo uma da outra
+        cameraOffset = mainCameraTransform.position - playerTransform.position;
+               
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        // Movendo a camera de acordo com o objeto usando o offset setado no começo
+        mainCameraTransform.position = playerTransform.position + cameraOffset;
+
+    }
+}
---
+++ Assets/Scripts/Player_movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_movement : MonoBehaviour {
+
+    public float velocidade;
+    public float velocidade_pulo;
+ 
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+        // Para a direita
+
+        if (Input.GetKey(KeyCode.A)) {
+
+            transform.Translate(0, velocidade, 0);
+
+        }
+
+        // Para Esquerda
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            transform.Translate(0, -velocidade, 0);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           // transform.Translate(0, 0, velocidade_pulo);
+        }
+
+    }
+}
