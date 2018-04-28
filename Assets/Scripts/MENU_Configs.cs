2d9d03592d56404b11ef1ea295bb6d8e502c7d77
leodelafiori <leodelafiori@users.noreply.github.com>
2018-04-28 17:10:42 -0300

    Criação de Menus ao apertar Esc.

--- Assets/Scripts/MENU_Configs.cs
+++ Assets/Scripts/MENU_Configs.cs
@@ -6,6 +6,7 @@ public class MENU_Configs : MonoBehaviour {
 
     public void MudaCena(string nome_cena)
     {
+        Time.timeScale = 1f;
         Application.LoadLevel(nome_cena);
     }
 }
---
+++ Assets/Scripts/Pause_menu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause_menu : MonoBehaviour {
+
+    public static bool Game_paused = false;
+    public GameObject pause_menu_UI;
+    public GameObject score_menu_UI;
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Game_paused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+
+        }
+
+    }
+
+        public void Resume()
+        {
+        pause_menu_UI.SetActive(false);
+        score_menu_UI.SetActive(true);
+        Time.timeScale = 1f;
+        Game_paused = false;
+        }
+
+        void Pause()
+        {
+        pause_menu_UI.SetActive(true);
+        score_menu_UI.SetActive(false);
+        Time.timeScale = 0f;
+        Game_paused = true;
+
+        }
+
+        public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+	
+}
