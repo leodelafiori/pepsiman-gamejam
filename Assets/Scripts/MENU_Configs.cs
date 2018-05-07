99938a8b781f1a975896618c7bc426d76b1349a8
leodelafiori <leodelafiori@users.noreply.github.com>
2018-05-07 14:06:00 -0300

    Alteracao Camera

--- Assets/Scripts/MENU_Configs.cs
+++ Assets/Scripts/MENU_Configs.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class MENU_Configs : MonoBehaviour {
 
+    public GameObject pause_menu_UI;
+    public GameObject score_menu_UI;
+    public GameObject bonus_menu_UI;
+
     public void MudaCena(string nome_cena)
     {
         Time.timeScale = 1f;
         Application.LoadLevel(nome_cena);
+    }
+
+    public void troca_bonus()
+    {
+        pause_menu_UI.SetActive(false);
+        bonus_menu_UI.SetActive(true);
+    }
+    public void troca_bonus_sair()
+    {
+        pause_menu_UI.SetActive(true);
+        bonus_menu_UI.SetActive(false);
     }
 
 }
